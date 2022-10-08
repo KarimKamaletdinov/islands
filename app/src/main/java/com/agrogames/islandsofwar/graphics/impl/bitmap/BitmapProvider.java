ce3c056f420eb0be4e6d91dffe63278258385b83
karim <450906k@gmail.com>
2022-10-08 21:27:24 +0500

    caching loaded bitmaps

--- app/src/main/java/com/agrogames/islandsofwar/graphics/impl/bitmap/BitmapProvider.java
+++ app/src/main/java/com/agrogames/islandsofwar/graphics/impl/bitmap/BitmapProvider.java
@@ -6,9 +6,12 @@ import android.graphics.BitmapFactory;
 import android.graphics.Color;
 
 import java.io.IOException;
+import java.util.Dictionary;
+import java.util.Hashtable;
 
 public class BitmapProvider {
     private final Context context;
+    private Dictionary<String, Bitmap> cachedBitmaps = new Hashtable<>();
 
     public BitmapProvider(Context context) {
         this.context = context;
@@ -16,7 +19,13 @@ public class BitmapProvider {
 
     public Bitmap Load(String name){
         try {
-            return BitmapFactory.decodeStream(context.getAssets().open("textures/" + name));
+            Bitmap cached = cachedBitmaps.get(name);
+            if(cached != null){
+                return cached;
+            }
+            Bitmap bitmap = BitmapFactory.decodeStream(context.getAssets().open("textures/" + name));
+            cachedBitmaps.put(name, bitmap);
+            return bitmap;
         } catch (IOException e) {
             return Bitmap.createBitmap(new int[]{Color.RED}, 1, 1, Bitmap.Config.RGB_565);
         }
